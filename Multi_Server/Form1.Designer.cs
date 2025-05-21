73e52624837578513be980949d21e7727b689176
jjoaa <59039168+jjoaa@users.noreply.github.com>
2025-05-21 15:56:59 +0900

    Multi_Server

---
+++ Multi_Server/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TCPServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtView = new System.Windows.Forms.TextBox();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("맑은 고딕", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(121, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Server";
+            // 
+            // txtView
+            // 
+            this.txtView.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.txtView.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtView.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtView.Location = new System.Drawing.Point(8, 39);
+            this.txtView.Multiline = true;
+            this.txtView.Name = "txtView";
+            this.txtView.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtView.Size = new System.Drawing.Size(314, 417);
+            this.txtView.TabIndex = 1;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("맑은 고딕", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtInput.Location = new System.Drawing.Point(8, 462);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(236, 87);
+            this.txtInput.TabIndex = 2;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnSend.Font = new System.Drawing.Font("맑은 고딕", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSend.Location = new System.Drawing.Point(250, 462);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(80, 87);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "전송";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(334, 561);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.txtView);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtView;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
